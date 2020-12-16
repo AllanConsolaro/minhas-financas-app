8d627de37896819d82d40335b424ccb945d1bbff
Allan Consolaro <aaconsolaro@gmail.com>
2020-12-16 15:16:16 -0300

    change config baseURL

--- src/app/apiservice.js
+++ src/app/apiservice.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://minhasfinancas-consolaro.herokuapp.com'
 })
 
 class ApiService {
--- src/main/provedorAutenticacao.js
+++ src/main/provedorAutenticacao.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import AuthService from '../app/service/authService'
 
 export const AuthContext = React.createContext();
 export const AuthConsumer = AuthContext.Consumer;
 const AuthProvider = AuthContext.Provider;
-
-import AuthService from '../app/service/authService'
 
 class ProvedorAutenticacao extends React.Component {
 
